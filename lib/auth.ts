a55e4cc0ad8c9c3c5330d2e11f89beeb932ae0ec
budicuy <personal.budicuy@gmail.com>
2025-11-21 16:17:09 +0800

    feat: Menambahkan konfigurasi Prisma dan mengintegrasikannya dengan NextAuth sebagai adapter.

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,5 @@
+import {PrismaAdapter} from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import {prisma} from "./prisma";
 
-export const {handlers, signIn, signOut, auth} = NextAuth({providers: []});
+export const {handlers, auth, signIn, signOut} = NextAuth({adapter: PrismaAdapter(prisma), providers: []});
